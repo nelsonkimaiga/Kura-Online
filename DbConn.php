c38b14773e851b31cd04c70d5487ba45c08031d0
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-14 16:49:39 +0300

    Authentication refactored to login.php

--- DbConn.php
+++ DbConn.php
@@ -1,35 +1,10 @@
-<!-- this code assists with database connectivity-->
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'iebc');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
-
-function SignIn()
+if(!mysql_connect("localhost","root",""))
 {
-session_start();   //starting the session for user profile page
-if(!empty($_POST['username']))   //checking the 'username' variable which is from index.html
+     die('oops connection problem ! --> '.mysql_error());
+}
+if(!mysql_select_db("iebc"))
 {
-	$query = mysql_query("SELECT *  FROM login where username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysql_error());
-	$row = mysql_fetch_array($query) or die(mysql_error());
-	if(!empty($row['username']) AND !empty($row['password']))
-	{
-		$_SESSION['username'] = $row['password'];
-		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
-
-	}
-	else
-	{
-		echo "SORRY... YOU ENTERED WRONG ID AND PASSWORD... PLEASE RETRY...";
-	}
+     die('oops database selection problem ! --> '.mysql_error());
 }
-}
-if(isset($_POST['submit']))
-{
-	SignIn();
-}
-
 ?>
--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+include_once 'DbConn.php';
+
+if(isset($_SESSION['user'])!="")
+{
+ header("Location: home.php");
+}
+if(isset($_POST['btn-login']))
+{
+ $usernamae = mysql_real_escape_string($_POST['user']);
+ $password = mysql_real_escape_string($_POST['pass']);
+ $res=mysql_query("SELECT * FROM login WHERE username='$username'");
+ $row=mysql_fetch_array($res);
+ if($row['password']==md5($password))
+ {
+  $_SESSION['user'] = $row['username'];
+  header("Location: home.php");
+ }
+ else
+ {
+  ?>
+        <script>alert('wrong details');</script>
+        <?php
+ }
+ 
+}
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,12 +40,12 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" type="text/css" href="styles/css.css">
+        <link rel="stylesheet" type="text/css" href="css.css">
     </head>
     <body>
     <!--header image with IEBC logo-->
 <header>
-  <img class="logo" src="images/iebc.jpg" alt="logo">
+  <img class="logo" src="iebc.jpg" alt="logo">
 </header>
 
 
@@ -25,17 +54,17 @@ and open the template in the editor.
           <h1>Login to Uchaguzi Online</h1>
           	
          
-        <form method="post" action="">
+        <form method="POST">
         <p>
         	<label for "username">Username: </label>
-        	<input type="text" id="username" class="field" placeholder="username"/>
+        	<input type="text" name="user" id="username" class="field" placeholder="username"/>
         </p>
         <p>
         	<label for="password">Password: </label>
-        	<input type="password" id="password" class="field" placeholder="password">
+        	<input type="password" name="pass" id="password" class="field" placeholder="password">
         </p>
         <div class="button">
-        <button id="submit" value="send credentials">Login</button>
+        <button id="submit" name = "btn-login" value="send credentials">Login</button>
         </div>
         <label for="">
         	<input type="checkbox" name="remember_me" id="remember_me">Remember me on this computer
